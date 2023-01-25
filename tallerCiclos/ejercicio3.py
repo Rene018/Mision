9230afc719da256a71e64a61699e6453f4e4b65b
Rene <rene18agosto@gmail.com>
2023-01-24 21:46:04 -0500

    commit

--- tallerCiclos/ejercicio3.py
+++ tallerCiclos/ejercicio3.py
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 operacion = -1
 while operacion != 0:
-    operacion = int(input(
-        'Que operacion desea realizar: \n1. Sumar 2 numeros. \n2. Restar 3 numeros. \n3. Multiplicar 2 numeros. \n4. Dividir 2 numeros. \n0. Salir de la calculadora. \n'))
+    operacion = int(input('Que operacion desea realizar: \n1. Sumar 2 numeros. \n2. Restar 3 numeros. \n3. Multiplicar 2 numeros. \n4. Dividir 2 numeros. \n0. Salir de la calculadora. \n'))
     if operacion < 0 or operacion > 5:
         print('ingrese un valor valido\n')
     elif operacion == 0:
@@ -37,36 +35,3 @@ while operacion != 0:
                 division = numero1/numero2
                 print('La divisioen de ', numero1,
                       ' y ', numero2, ' es: ', division)
-=======
-operacion=-1
-while operacion!=0:
-	print('Que operacion desea realizar: ')
-	print('1. Sumar 2 numeros. \n2. Restar 3 numeros. \n3. Multiplicar 2 numeros. \n4. Dividir 2 numeros. \n0. Salir de la calculadora.')
-	operacion =int( input("Ingrese la operacion que desea realizar: "))
-	if operacion<0 or operacion>5:
-		print('ingrese un valor valido\n')
-	elif operacion==1:
-		numero1=int( input("ingrese un numero: "))
-		numero2=int( input("ingrese un numero: "))
-		sumatoria=numero1+numero2
-		print('La suma de ',numero1,' y ',numero2,' es: ', sumatoria)
-	elif operacion==2:
-		numero1=int( input("ingrese un numero: "))
-		numero2=int( input("ingrese un numero: "))
-		numero3=int( input("ingrese un numero: "))
-		diferencia=numero1-numero2-numero3
-		print('La diferencia entre ',numero1,', ',numero2,'y ',numero3,' es: ',diferencia)
-	elif operacion==3:
-		numero1=int( input("ingrese un numero: "))
-		numero2=int( input("ingrese un numero: "))
-		producto=numero1*numero2
-		print('El producto entre ',numero1,' y ',numero2,' es: ',producto)
-	elif operacion==4:
-		numero1=int( input("Ingrese un numero: "))
-		numero2=int( input("Ingrese un numero: "))
-		if numero2==0:
-			print('Ingrese un valor valido')
-		else:
-			division=numero1/numero2
-			print('el entre',numero1,' y ',numero2,' es: ', sumatoria)
->>>>>>> 0ade36344b536f0880f32299249e5a897d4ccf50
