2d29273becb4719104c5c7ad2f4f0e0e097d35a9
Rene <rene18agosto@gmail.com>
2023-03-01 21:40:35 -0500

    co

--- MODULO 2/main.js
+++ MODULO 2/main.js
@@ -7,9 +7,11 @@ let $poke = document.querySelector('#poke')
 async function buscar() {
     const response = await fetch(api+$poke.value)
     const data= await response.json()
-    // let {front_default}= data.sprites
     let {front_default}=data.sprites.other.home
-    $container.innerHTML= `<img src="${front_default}">`
+    let {name}= data.types[0].type
+    $container.innerHTML= `<div><h1>${data.name}</h1></div>
+    <img src="${front_default}">
+    <div><h1>${name}</h1></div>`
     console.log(data.sprites);
 }
 $buscar.addEventListener('click', buscar)
