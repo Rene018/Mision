439482006b2e58035c0adcc8abeb9c6adb4ba039
rene018 <rene18agosto@gmail.com>
2023-03-10 13:54:50 -0500

    commit

--- PROYECTO/prueba.js
+++ PROYECTO/prueba.js
@@ -1,13 +1,76 @@
-let palabras = ['perro', "hola"]
-let intentosMaximos = 6;
+let palabras = ['perro', 'luna']
+let intentosMaximos = 8;
 let intentosFa = 0;
 let letrasCorrectas = [];
-let palabra = 0
+let letrasIncorrectas = []
+let numero = 0
+let palabra = seleccionarPalabra()
 let finDelJuego = false;
 
+
+
 function seleccionarPalabra() {
-    return palabras[Math.floor(Math.random() * palabras.length)];
+    numero = Math.floor(Math.random() * palabras.length)
+    return palabras[numero];
 }
 function dibujarAhorcado() {
-    document.querySelector(`#line-${intentosFa}`).classList.add('visible');
+    if (intentosFa <intentosMaximos) {
+        document.querySelector(`#line-${intentosFa}`).classList.add('visible');
+        console.log('hola');
+    }
 }
+function recorrerPalabra(letra) {
+    if ((letrasCorrectas.includes(letra)) || (letrasIncorrectas.includes(letra))) {
+
+    } else {
+        for (let index = 0; index < palabra.length; index++) {
+            const element = palabra[index];
+            if (element == letra) {
+                letrasCorrectas.push(element)
+            } else {
+                if (!letrasIncorrectas.includes(letra) && !palabra.includes(letra)) {
+                    letrasIncorrectas.push(letra)
+                    intentosFa += 1
+                }
+
+            }
+
+        }
+        console.log(letrasCorrectas);
+        console.log(letrasIncorrectas);
+    }
+
+}
+function removv() {
+    document.querySelector('.dibujando').classList.remove(".visible");
+    
+}
+function validarIntento() {
+    if (letrasCorrectas.length == palabra.length || (intentosFa>=intentosMaximos)) {
+        removv()
+        finDelJuego = true
+        palabras.splice(numero, 1)
+        console.log(palabras);
+        palabra = seleccionarPalabra()
+        intentosMaximos = 7;
+        intentosFa = 0;
+        letrasCorrectas = [];
+        letrasIncorrectas = []
+        
+
+    }
+}
+
+
+document.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13) {
+        var $entrada = document.getElementById("entrada").value;
+        recorrerPalabra($entrada)
+        validarIntento()
+        dibujarAhorcado()
+        console.log(intentosFa);
+
+
+        document.getElementById("formulario").reset();
+    }
+});
